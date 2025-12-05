8938075b1a4571cd0e98a3f3992ff8af87f12023
cineph <103133024+cineph@users.noreply.github.com>
2025-12-05 18:12:42 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,4 +1,4 @@
-
+sed -i 's/192.168.50.10/10.0.0.1/g' package/base-files/files/bin/config_generate
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
